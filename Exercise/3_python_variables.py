c8c159f9642ca7950b388160586b62a93e207417
mo0lig <igalagik@gmail.com>
2023-01-21 15:10:28 +0600

    Python Variables

---
+++ Exercise/3_python_variables.py
@@ -0,0 +1,27 @@
+#Exercise 1
+carname = "Volvo"
+
+#Exercise 2
+x = 50
+
+#Exercise 3
+x = 5
+y = 10
+print(x + y)
+
+#Exercise 4
+x = 5
+y = 10
+z = x + y
+print(z)
+
+#Exercise 5
+myfirst_name = "John"
+
+#Exercise 6
+x = y = z = "Orange"
+
+#Exercise 7
+def myfunc():
+    global x
+    x = "fantastic"
