2124961e6cb943e8b520950e9f966a667acef868
mo0lig <igalagik@gmail.com>
2023-01-21 17:04:12 +0600

    Python Functions

---
+++ Exercise/16_python_functions.py
@@ -0,0 +1,25 @@
+#Exercise 1
+def my_function():
+    print("Hello from a function")
+
+#Exercise 2
+def my_function():
+    print("Hello from a function")
+
+my_function()
+
+#Exercise 3
+def my_function(fname, lname):
+    print(fname)
+
+#Exercise 4
+def my_function(x):
+    return x + 5
+
+#Exercise 5
+def my_function(*kids):
+    print("The youngest child is " + kids[2])
+
+#Exercise 6
+def my_function(kid):
+    print("His last name is " + kid["lname"])
