48c0f6c4eaca1e6dcf289b2c0f22c7b1a013916b
mo0lig <igalagik@gmail.com>
2023-01-21 15:28:00 +0600

    Python Booleans

---
+++ Exercise/7_python_booleans.py
@@ -0,0 +1,21 @@
+#Exercise 1
+print(10 > 9)
+#Answer => True
+
+#Exercise 2
+print(10 == 9)
+# Answer => False
+
+#Exercise 3
+print(10 < 9)
+# Answer => False
+
+#Exercise 4
+print(bool("abc"))
+# Answer => True
+
+#Exercise 5
+print(bool(0))
+#Answer => False
+
+
