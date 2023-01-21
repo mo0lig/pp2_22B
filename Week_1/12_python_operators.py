79729d47a60281c9944208181674c7a01f5dbdba
mo0lig <igalagik@gmail.com>
2023-01-22 01:03:20 +0600

    Python Operators

---
+++ Week_1/12_python_operators.py
@@ -0,0 +1,116 @@
+# + operator Addition
+print(10 + 5) #15
+
+# - operator Subtarction
+x = 5
+y = 3
+print(x - y) #2
+
+# * operator Multiplication
+x = 5
+y = 3
+print(x * y) #15 
+
+# / operator Division
+x = 12
+y = 3
+print(x / y) #4
+
+# % operator Modulus
+x = 5
+y = 2
+print(x % y) #1
+
+# ** operator Exponentiation
+x = 2
+y = 5
+print(x ** y) #same as 2*2*2*2*2
+
+# // operator FLoor division
+x = 15
+y = 2
+print(x // y) #7
+
+#1)
+x = 5
+print(x) #5
+#2)
+x = 5
+x += 3
+print(x) #8
+#3)
+x = 5
+x -= 3
+print(x) #2
+#4)
+x = 5
+x *= 3
+print(x) #15
+#5)
+x = 5
+x /= 3
+print(x) #1.666666666667
+#6)
+x = 5
+x%=3
+print(x) #2
+#7)
+x = 5
+x//=3
+print(x) #1
+#8)
+x = 5
+x &= 3
+print(x) #1
+#9)
+x = 5
+x |= 3
+print(x) #7
+#10)
+x = 5
+x ^= 3
+print(x) #6
+#11)
+x = 5
+x >>= 3
+print(x) #0
+#12)
+x = 5
+x <<= 3
+print(x) #40
+
+#Python Comparison Operators
+x = 5
+y = 3
+print(x == y) #False
+
+x = 5
+y = 3
+print(x != y) #True
+
+x = 5
+y = 3
+print(x > y) #True
+
+x = 5
+y = 3
+print(x < y) #False
+
+x = 5
+y = 3
+print(x >= y) #True
+
+x = 5
+y = 3
+print(x <= y) #False
+
+#Python Logical Operators
+x = 5
+print(x > 3 and x < 10) #True
+
+x = 5
+print(x > 3 or x < 4) #True
+
+x = 5
+print(not(x > 3 and x < 10)) #False
+ 
