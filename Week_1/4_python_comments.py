764dc8ac5810137a29672d1b4a747e5c3f3741cf
mo0lig <igalagik@gmail.com>
2023-01-21 18:23:08 +0600

    Python Comments

---
+++ Week_1/4_python_comments.py
@@ -0,0 +1,25 @@
+#Creating a comment, it starts from #
+
+#Example of comment:
+#1)
+#This is a comment
+print("Hello, World!")
+#2)
+print("Hello, World!") #This is a comment
+#3)
+#print("Hello, World!")
+print("Cheers, Mate!")
+
+#Multiline comments example:
+#1)
+#This is a comment
+#written in
+#more than just one line
+print("Hello, World!")
+#2)
+"""
+This is a comment
+written in
+more than just one line
+"""
+print("Hello, World!")
