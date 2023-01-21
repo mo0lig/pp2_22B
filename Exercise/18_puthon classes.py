ae5b984f0472d69d770b2c4766b9c910e995f7e8
mo0lig <igalagik@gmail.com>
2023-01-21 17:09:14 +0600

    Python Classes

---
+++ Exercise/18_puthon classes.py
@@ -0,0 +1,21 @@
+#Exercise 1
+class MyClass:
+    x = 5
+
+#Exercise 2
+class MyClass:
+    x = 5
+p1 = MyClass()
+
+#Exercise 3
+class MyClass:
+    x = 5
+
+p1 = MyClass()
+print(p1.x)
+
+#Exercise 4
+class Person:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
