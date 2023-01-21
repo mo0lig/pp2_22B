166ad684895ef778ae9ea110fa88dd3bcb3461df
mo0lig <igalagik@gmail.com>
2023-01-21 21:32:31 +0600

    Python Data Types

---
+++ Week_1/6_python_data_types.py
@@ -0,0 +1,79 @@
+#Getting the data type
+x = 5
+print(type(x))
+
+#Data types:
+#1) STR
+x = "Hello World"
+print(x)
+print(type(x)) 
+
+#2) INT
+x = 20
+print(x)
+print(type(x)) 
+
+#3) FLOAT
+x = 20.5
+print(x)
+print(type(x)) 
+
+#4) COMPLEX
+x = 1j
+print(x)
+print(type(x)) 
+
+#5) LIST
+x = ["apple", "banana", "cherry"]
+print(x)
+print(type(x)) 
+
+#6) TUPLE
+x = ("apple", "banana", "cherry")
+print(x)
+print(type(x)) 
+
+#7) RANGE
+x = range(6)
+print(x)
+print(type(x)) 
+
+#8) DICT
+x = {"name" : "John", "age" : 36}
+print(x)
+print(type(x)) 
+
+#9) SET
+x = {"apple", "banana", "cherry"}
+print(x)
+print(type(x)) 
+
+#10) FROZENSET
+x = frozenset({"apple", "banana", "cherry"})
+print(x)
+print(type(x)) 
+
+#11) BOOL
+x = True
+print(x)
+print(type(x)) 
+
+#12) BYTES
+x = b"Hello"
+print(x)
+print(type(x)) 
+
+#13) BYTEARRAY
+x = bytearray(5)
+print(x)
+print(type(x)) 
+
+#14) MEMORYVIEW
+x = memoryview(bytes(5))	
+print(x)
+print(type(x)) 
+
+#15) NONE TYPE
+x = None	
+print(x)
+print(type(x)) 
