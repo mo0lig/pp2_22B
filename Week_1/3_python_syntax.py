778ff14df40a4e2097d8ae3dd9dbc82680310f36
mo0lig <igalagik@gmail.com>
2023-01-21 18:19:39 +0600

    Python Syntax

---
+++ Week_1/3_python_syntax.py
@@ -0,0 +1,16 @@
+#Python Indentation
+if 5 > 2:
+    print("Five is greater than two!")
+
+
+"""
+Syntax Error:
+if 5 > 2:
+print("Five is greater than two!")
+"""
+
+#Example of number of spaces
+if 5 > 2:
+    print("Five is greater than two!")  #It can be 1
+if 5 > 2:
+        print("Five is greater than two!") #or 2,also most common is 4
