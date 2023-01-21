f499d4e09a29605f0f7380aa949362638f404d16
mo0lig <igalagik@gmail.com>
2023-01-21 15:42:13 +0600

    Python Sets

---
+++ Exercise/11_python_sets.py
@@ -0,0 +1,21 @@
+#Exercise 1
+fruits = {"apple", "banana", "cherry"}
+if "apple" in fruits:
+    print("Yes, apple is a fruit!")
+
+#Exercise 2
+fruits = {"apple", "banana", "cherry"}
+fruits.add("orange")
+
+#Exercise 3
+fruits = {"apple", "banana", "cherry"}
+more_fruits = ["orange", "mango", "grapes"]
+fruits.update(more_fruits)
+
+#Exercise 4
+fruits = {"apple", "banana", "cherry"}
+fruits.remove("banana")
+
+#Exercise 5
+fruits = {"apple", "banana", "cherry"}
+fruits.discard("banana")
