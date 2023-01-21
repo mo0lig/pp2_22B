7d9385d3954b2e03333a2552b490f2e283a73d83
mo0lig <igalagik@gmail.com>
2023-01-21 15:49:51 +0600

    Python If&Else

---
+++ Exercise/13_python_if_else.py
@@ -0,0 +1,47 @@
+#Exercise 1
+a = 50
+b = 10
+if a > b:
+    print("Hello World")
+
+#Exercise 2
+a = 50
+b = 10
+if a != b:
+    print("Hello World")
+
+#Exercise 3
+a = 50
+b = 10
+if a == b:
+    print("Yes")
+else:
+    print("No")
+
+#Exercise 4
+a = 50
+b = 10
+if a == b:
+    print("1")
+elif a > b:
+    print("2")
+else:
+    print("3")
+
+#Exercise 5
+if a == b and c == d:
+    print("Hello")
+
+#Exercise 6
+if a == b or c == d:
+    print("Hello")
+
+#Exercise 7
+if 5 > 2:
+    print("Five is greater than two!")
+
+#Exercise 8
+if 5 > 2: print("Five is greater than two!")
+
+#Exercise 9
+print("Yes") if 5 > 2 else print("No")
