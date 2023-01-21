775bd31bb36ab158b85d9c84068cbfdb8a2b4ec3
mo0lig <igalagik@gmail.com>
2023-01-21 15:15:54 +0600

    Python Data Types

---
+++ Exercise/4_python_data_types.py
@@ -0,0 +1,42 @@
+#Exercise 1
+x = 5
+print(type(x))
+# Answer => int
+
+#Exercise 2
+x = "Hello World"
+print(type(x))
+# Answer => str
+
+#Exercise 3
+x = 20.5
+print(type(x))
+# Answer => float
+
+#Exercise 4
+x = ["apple", "banana", "cherry"]
+print(type(x))
+# Answer => list
+
+#Exercise 5
+x = ("apple", "banana", "cherry")
+print(type(x))
+#Answer => tuple
+
+#Exercise 6
+x = {"name" : "John", "age" : 36}
+print(type(x))
+#Answer => dict
+
+#Exercise 7
+x = True
+print(type(x))
+#Answer => bool
+
+
+
+
+
+
+
+
