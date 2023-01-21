db191ce6345086fc2cac7e08053ed46a33c1dbb2
mo0lig <igalagik@gmail.com>
2023-01-21 15:23:49 +0600

    Python String

---
+++ Exercise/6_python_string.py
@@ -0,0 +1,35 @@
+#Exercise 1
+x = "Hello World"
+print(len(x))
+
+#Exercise 2
+txt = "Hello World"
+x = txt[0]
+
+#Exercise 3
+txt = "Hello World"
+x = txt[2:5]
+
+#Exercise 4
+txt = " Hello World "
+x = txt.strip()
+
+#Exercise 5
+txt = "Hello World"
+txt = txt.upper()
+
+#Exercise 6
+txt = "Hello World"
+txt = txt.lower()
+
+#Exercise 7
+txt = "Hello World"
+txt = txt.replace("H", "J")
+
+#Exercise 8
+age = 36
+txt = "My name is John, and I am {}"
+print(txt.format(age))
+
+
+
